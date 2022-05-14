5cdd9b5d416d5ef7def9a560667e30d34f821017
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-14 18:53:50 -0500

    feat: add constants

---
+++ app/src/main/java/dev/adds/placetopay/util/Constants.kt
@@ -0,0 +1,8 @@
+package dev.adds.placetopay.util
+
+object Constants {
+    const val PLACE_TO_PAY_API = "https://dev.placetopay.com/rest/"
+    const val PRODUCT_MOCK_API = "https://6264408aa55d5055be46e83a.mockapi.io/"
+    const val API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mmZ"
+    const val ALGORITHM_API = "SHA-256"
+}
