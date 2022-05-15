4db7300f739b59b65562a604d3ccd8a1c150cc0a
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-14 19:23:01 -0500

    refactor: home view model

--- app/src/main/java/dev/adds/placetopay/ui/home/HomeViewModel.kt
+++ app/src/main/java/dev/adds/placetopay/ui/home/HomeViewModel.kt
@@ -3,11 +3,30 @@ package dev.adds.placetopay.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.adds.placetopay.model.domain.Product
+import dev.adds.placetopay.usescase.GetProducts
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val productsList = MutableLiveData<List<Product>>()
+
+    val isLoading = MutableLiveData<Boolean>()
+
+    var getProducts = GetProducts()
+
+    fun onCreate(){
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            val result  = getProducts()
+            if(!result.isNullOrEmpty()) {
+                productsList.postValue(result!!)
+                isLoading.postValue(false)
+            }
+        }
     }
-    val text: LiveData<String> = _text
+
+    val products : LiveData<List<Product>> = productsList
+
 }
