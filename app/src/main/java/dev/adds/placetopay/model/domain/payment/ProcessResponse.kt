b32eff151b6691343c6da87352c016d8f0f720a3
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-15 16:20:51 -0500

    refactor: process repository

--- app/src/main/java/dev/adds/placetopay/model/domain/payment/ProcessResponse.kt
+++ app/src/main/java/dev/adds/placetopay/model/domain/payment/ProcessResponse.kt
@@ -1,3 +1,7 @@
 package dev.adds.placetopay.model.domain.payment
 
-data class ProcessResponse(var status: Status)
+import dev.adds.placetopay.model.domain.Amount
+
+data class ProcessResponse(var status: Status, var provider: String,var internalReference: Long,
+                           var reference: String, var paymentMethod: String, var amount: Amount,
+                           var authorization: String)
