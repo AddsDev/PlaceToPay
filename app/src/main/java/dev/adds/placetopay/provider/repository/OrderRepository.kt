4ee9451278956df3801b40aab04eb0cae53c6471
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-14 19:43:38 -0500

    feat: add order repository

---
+++ app/src/main/java/dev/adds/placetopay/provider/repository/OrderRepository.kt
@@ -0,0 +1,20 @@
+package dev.adds.placetopay.provider.repository
+
+import dev.adds.placetopay.model.domain.Card
+import dev.adds.placetopay.model.domain.Instrument
+import dev.adds.placetopay.model.domain.Payer
+import dev.adds.placetopay.model.domain.Payment
+import dev.adds.placetopay.model.domain.payment.Process
+import dev.adds.placetopay.provider.OrderProvider
+
+
+class OrderRepository {
+
+    fun newOrder(payer: Payer, payment: Payment, card: Card){
+        OrderProvider.process = Process(payer, payment, card)
+    }
+    fun getOrder(): Process{
+        return  OrderProvider.process!!
+    }
+    fun getAllOrders() = OrderProvider.orders
+}
