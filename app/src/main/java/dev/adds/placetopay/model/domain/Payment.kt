51b8a515ca732a4369f243d5c2b936a05f0b0bcc
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-15 14:17:14 -0500

    refactor: update fields models

--- app/src/main/java/dev/adds/placetopay/model/domain/Payment.kt
+++ app/src/main/java/dev/adds/placetopay/model/domain/Payment.kt
@@ -1,7 +1,6 @@
 package dev.adds.placetopay.model.domain
 
-import java.util.*
 
-data class Payment(var referece: String, var description: String)
+data class Payment(var referece: String, var description: String, var amount: Amount)
 
 data class Amount(var currency: String, var total: Int)
