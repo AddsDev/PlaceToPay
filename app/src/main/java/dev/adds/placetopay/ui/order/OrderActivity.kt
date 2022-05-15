ca021bad82f9584cfce0deacc649300d54a666e3
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-14 19:41:55 -0500

    feat: add order activity

---
+++ app/src/main/java/dev/adds/placetopay/ui/order/OrderActivity.kt
@@ -0,0 +1,12 @@
+package dev.adds.placetopay.ui.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.adds.placetopay.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
