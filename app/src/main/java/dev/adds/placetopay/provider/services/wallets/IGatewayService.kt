4d5162ee653a9799fa961ce031cb8371877e9533
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-15 16:24:19 -0500

    refactor: implement suspend function

--- app/src/main/java/dev/adds/placetopay/provider/services/wallets/IGatewayService.kt
+++ app/src/main/java/dev/adds/placetopay/provider/services/wallets/IGatewayService.kt
@@ -1,9 +1,11 @@
 package dev.adds.placetopay.provider.services.wallets
 
 import dev.adds.placetopay.model.domain.payment.ProcessResponse
+import dev.adds.placetopay.model.domain.payment.Process
 import retrofit2.http.POST
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Headers
 
@@ -15,5 +17,5 @@ interface IGatewayService {
 
     @Headers("Content-Type: application/json")
     @POST("gateway/process")
-    fun getTransaction(@Body process: Process): Call<ProcessResponse>
+    suspend fun getTransaction(@Body process: Process): Response<ProcessResponse>
 }
