6aa965d7fc076ec670300fa7dc3ef9edf51a55f1
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-15 16:21:23 -0500

    refactor: update service instance

--- app/src/main/java/dev/adds/placetopay/provider/repository/ProductRepository.kt
+++ app/src/main/java/dev/adds/placetopay/provider/repository/ProductRepository.kt
@@ -3,11 +3,11 @@ package dev.adds.placetopay.provider.repository
 import dev.adds.placetopay.core.UrlApi
 import dev.adds.placetopay.model.domain.Product
 import dev.adds.placetopay.provider.ProductProvider
-import dev.adds.placetopay.provider.services.products.ProductService
+import dev.adds.placetopay.provider.services.ApiService
 
 class ProductRepository {
 
-    private val api = ProductService(api = UrlApi.PRODUCT)
+    private val api = ApiService(api = UrlApi.PRODUCT)
 
     suspend fun getAllProducts() : List<Product>{
         val response = api.getProducts()
