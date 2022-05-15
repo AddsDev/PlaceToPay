c67d56b7d82c187b50cdfdca188808ed766db146
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-15 14:24:42 -0500

    refactor: extends viewmodel

--- app/src/main/java/dev/adds/placetopay/ui/order/OrderViewModel.kt
+++ app/src/main/java/dev/adds/placetopay/ui/order/OrderViewModel.kt
@@ -2,19 +2,24 @@ package dev.adds.placetopay.ui.order
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import dev.adds.placetopay.model.domain.Card
 import dev.adds.placetopay.model.domain.Payer
 import dev.adds.placetopay.model.domain.Payment
+import dev.adds.placetopay.model.domain.payment.Process
+import dev.adds.placetopay.usescase.ManagementOrder
 
-class OrderViewModel {
+class OrderViewModel : ViewModel() {
 
     private val payer_ = MutableLiveData<Payer>()
     private val payment_ = MutableLiveData<Payment>()
     private val card_ = MutableLiveData<Card>()
+    private val order_ = MutableLiveData<Process>()
 
     val payer : LiveData<Payer> = payer_
     val payment : LiveData<Payment> = payment_
     val card: LiveData<Card> = card_
+    val order: LiveData<Process> = order_
 
     fun setPayer(payer: Payer){
         payer_.value = payer
@@ -24,6 +29,15 @@ class OrderViewModel {
     }
     fun setPayment(payment: Payment){
         payment_.value = payment
+    }
+
+    fun processOrder(){
+        ManagementOrder().newOrder(payer_.value!!, payment_.value!!, card_.value!!)
+    }
+
+    fun getOrder() : Process {
+        order_.value = ManagementOrder().getOrder()
+        return order_.value!!
     }
 
 
