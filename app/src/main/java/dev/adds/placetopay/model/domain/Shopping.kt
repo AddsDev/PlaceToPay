535785f4cdc4af77eda8420afe027b99825a3cb0
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-15 21:23:27 -0500

    refactor: add import process

--- app/src/main/java/dev/adds/placetopay/model/domain/Shopping.kt
+++ app/src/main/java/dev/adds/placetopay/model/domain/Shopping.kt
@@ -1,5 +1,6 @@
 package dev.adds.placetopay.model.domain
 
 import dev.adds.placetopay.model.domain.payment.ProcessResponse
+import dev.adds.placetopay.model.domain.payment.Process
 
 data class Shopping(var process: Process, var processResponse: ProcessResponse, var payer: Payer, var card: Card)
