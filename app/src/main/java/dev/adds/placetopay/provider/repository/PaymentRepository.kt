1c739167460faa17cb4890490704d7da15b00cc0
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-15 21:03:35 -0500

    refactor: update payment provider

--- app/src/main/java/dev/adds/placetopay/provider/repository/PaymentRepository.kt
+++ app/src/main/java/dev/adds/placetopay/provider/repository/PaymentRepository.kt
@@ -1,8 +1,10 @@
 package dev.adds.placetopay.provider.repository
 
 import dev.adds.placetopay.core.UrlApi
+import dev.adds.placetopay.model.domain.Shopping
 import dev.adds.placetopay.model.domain.payment.Process
 import dev.adds.placetopay.model.domain.payment.ProcessResponse
+import dev.adds.placetopay.provider.PaymentProvider
 import dev.adds.placetopay.provider.ProcessProvider
 import dev.adds.placetopay.provider.services.ApiService
 
@@ -13,4 +15,11 @@ class PaymentRepository {
         ProcessProvider.process.add(response)
         return response
     }
+    fun getAllPayments(): List<Shopping>{
+        return PaymentProvider.shopping
+    }
+    fun addPayment(shopping: Shopping){
+        PaymentProvider.shopping.add(shopping)
+    }
+    fun removePayment(shopping: Shopping): Boolean = PaymentProvider.shopping.remove(shopping)
 }
