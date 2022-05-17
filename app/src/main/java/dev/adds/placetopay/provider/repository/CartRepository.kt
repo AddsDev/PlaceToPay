3ed8c5b23ab58e4a4631f1c63aab26b154f81ea0
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-17 09:57:39 -0500

    refactor: update cart repository

--- app/src/main/java/dev/adds/placetopay/provider/repository/CartRepository.kt
+++ app/src/main/java/dev/adds/placetopay/provider/repository/CartRepository.kt
@@ -1,33 +1,49 @@
 package dev.adds.placetopay.provider.repository
 
+import android.util.Log
+import dev.adds.placetopay.model.database.dao.CartDao
+import dev.adds.placetopay.model.database.entities.CartEntity
 import dev.adds.placetopay.model.domain.ProductModel
 import dev.adds.placetopay.provider.ProductProvider
+import dev.adds.placetopay.usescase.model.ProductItem
+import dev.adds.placetopay.usescase.model.toDomain
 import javax.inject.Inject
 
 class CartRepository @Inject constructor(
-    private val productProvider: ProductProvider
+    private val productProvider: ProductProvider,
+    private val dao: CartDao
 ){
 
-    fun getAllProducts(): List<ProductModel>{
-        return  productProvider.cart
+    fun getAllProducts(): List<ProductItem>{
+        return  productProvider.cart.map { it.toDomain() }
     }
 
-    fun addProduct(productModel: ProductModel){
-        productProvider.cart.add(productModel)
+    suspend fun addProduct(productItem: ProductItem){
+        productProvider.cart.add(productItem.toModel())
+        val id = dao.insert(CartEntity(name = productItem.name, img = productItem.img, price = productItem.price))
     }
 
-    fun removeProduct(productModel: ProductModel): Boolean{
-        return productProvider.cart.remove(productModel)
+    suspend fun removeProduct(productItem: ProductItem): Boolean{
+        productProvider.cart.remove(productItem.toModel())
+        return dao.remove(CartEntity(productItem.id!!,productItem.name, productItem.img, productItem.price)) > 0
     }
 
     fun removeProduct(index: Int){
         productProvider.cart.removeAt(index)
     }
-    fun clean(){
+    suspend fun clean(){
         productProvider.cart.clear()
+        dao.remove()
     }
 
     fun getTotal(): Float {
         return productProvider.cart.map {it.price }.sumOf { fl: Float? -> fl!!.toInt() }.toFloat()
+    }
+
+    suspend fun getAllProductsCart(): List<ProductItem> {
+        productProvider.cart.clear()
+        val response = dao.getAllProducts()
+        productProvider.cart.addAll(response.map { it.toDomain().toModel() })
+        return productProvider.cart.map { it.toDomain() }
     }
 }
--- app/src/main/java/dev/adds/placetopay/provider/repository/PaymentRepository.kt
+++ app/src/main/java/dev/adds/placetopay/provider/repository/PaymentRepository.kt
@@ -1,39 +1,61 @@
 package dev.adds.placetopay.provider.repository
 
 import dev.adds.placetopay.model.database.dao.TransactionDao
-import dev.adds.placetopay.model.database.entities.TransactionEntity
-import dev.adds.placetopay.model.domain.payment.ProcessModel
+import dev.adds.placetopay.model.database.entities.*
 import dev.adds.placetopay.provider.PaymentProvider
 import dev.adds.placetopay.provider.ProcessProvider
 import dev.adds.placetopay.provider.services.ApiService
-import dev.adds.placetopay.usescase.model.ShoppingItem
-import dev.adds.placetopay.usescase.model.ProcessItem
-import dev.adds.placetopay.usescase.model.ProcessResponseItem
-import dev.adds.placetopay.usescase.model.toDomain
+import dev.adds.placetopay.usescase.model.*
 import javax.inject.Inject
 
 class PaymentRepository @Inject constructor(
     private val api: ApiService,
     private val paymentProvider: PaymentProvider,
     private val processProvider: ProcessProvider,
-    private val transactionDao: TransactionDao
+    private val dao: TransactionDao
 ) {
 
-    suspend fun getProcessResponse(processItem: ProcessItem) : ProcessResponseItem {
-        val response = api.getProcessResponse(processItem.toModel())// pasar modelo
-        //transactionDao.insert(TransactionEntity(null, response.statusModel))
-        processProvider.process.add(response)
+    suspend fun getProcessResponse(processItem: ProcessItem) : ProcessResponseItem =
+        api.getProcessResponse(processItem.toModel()).toDomain()
 
-        return response.toDomain()
+
+    suspend fun register(response: ProcessResponseItem): Long {
+        return dao.insert(TransactionEntity(provider = response.provider?: String(),
+            reference = response.reference))
     }
-    fun getAllPayments(): List<ShoppingItem>{
-        return paymentProvider.shoppingModel.map { it.toDomain() }
+    suspend fun register(amountItem: AmountItem, transactionId: Long): Long{
+        return  dao.insert(AmountEntity(currency = amountItem.currency ,
+            total = amountItem.total, transactionOwnerId = transactionId))
+    }
+    suspend fun register(cardItem: CardItem, transactionId: Long): Long =
+        dao.insert(CardEntity(number = cardItem.number,
+            expiration = cardItem.expiration,
+            transactionOwnerId = transactionId))
+
+    suspend fun register(payerItem: PayerItem, transactionId: Long): Long =
+        dao.insert(PayerEntity(name = payerItem.name,
+            surname = payerItem.surname,
+            email = payerItem.email,
+            documentType = payerItem.documentType,
+            document = payerItem.document,
+            mobile = payerItem.mobile, transactionOwnerId = transactionId))
+
+    suspend fun register(statusItem: StatusItem, transactionId: Long): Long =
+        dao.insert(StatusEntity(status = statusItem.status,
+            message = statusItem.message,
+            reason = statusItem.reason,
+            date = statusItem.date, transactionOwnerId = transactionId))
+
+    suspend fun getAllPayments(): List<ShoppingItem>{
+        return dao.getAllTransactions().map { it.toDomain() }
     }
 
     fun addPayment(shoppingItem: ShoppingItem){
         paymentProvider.shoppingModel.add(shoppingItem.toModel())
     }
 
-    fun removePayment(shoppingItem: ShoppingItem): Boolean =
-        paymentProvider.shoppingModel.remove(shoppingItem.toModel())
+    suspend fun removePayment(shoppingItem: ShoppingItem): Boolean {
+        val transaction = dao.getTransactionByReference(shoppingItem.processResponseItem.reference)
+        return dao.remove(transaction.transaction) > 0
+    }
 }
