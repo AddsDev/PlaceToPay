0b9f0a3f539a3be9b17a2c2962110a447b8be74f
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-17 11:37:11 -0500

    refactor: add config http client retrofit

--- app/src/main/java/dev/adds/placetopay/di/RetrofitModule.kt
+++ app/src/main/java/dev/adds/placetopay/di/RetrofitModule.kt
@@ -7,8 +7,11 @@ import dagger.hilt.components.SingletonComponent
 import dev.adds.placetopay.provider.services.products.IProductService
 import dev.adds.placetopay.provider.services.wallets.IGatewayService
 import dev.adds.placetopay.util.Constants
+import okhttp3.ConnectionPool
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -17,9 +20,15 @@ object RetrofitModule {
     @Singleton
     @Provides
     fun providePlaceToPay(): Retrofit{
+        val client = OkHttpClient.Builder()
+            .connectTimeout(Constants.CONNECT_TIME_OUT_API, TimeUnit.SECONDS)
+            .readTimeout(Constants.READ_TIME_OUT_API, TimeUnit.SECONDS)
+            .connectionPool(ConnectionPool(0, Constants.POOL_TIME_OUT_API, TimeUnit.MINUTES))
+            .build()
         return Retrofit.Builder()
             .baseUrl(Constants.PLACE_TO_PAY_API)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 
