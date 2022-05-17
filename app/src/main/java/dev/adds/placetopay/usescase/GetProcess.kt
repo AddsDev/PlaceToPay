ffa9ae0c01132e3046d2f3f5369ef484d2d3e102
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-17 10:03:26 -0500

    refactor: save transaction

--- app/src/main/java/dev/adds/placetopay/usescase/GetProcess.kt
+++ app/src/main/java/dev/adds/placetopay/usescase/GetProcess.kt
@@ -1,5 +1,6 @@
 package dev.adds.placetopay.usescase
 
+import android.util.Log
 import dev.adds.placetopay.provider.repository.PaymentRepository
 import dev.adds.placetopay.usescase.model.ProcessResponseItem
 import dev.adds.placetopay.usescase.model.ProcessItem
@@ -9,6 +10,14 @@ class GetProcess @Inject constructor(
     private val  repository: PaymentRepository
 ) {
 
-    suspend operator fun invoke(process: ProcessItem): ProcessResponseItem =
-        repository.getProcessResponse(process)
+    suspend operator fun invoke(process: ProcessItem): ProcessResponseItem {
+        return repository.getProcessResponse(process).apply {
+            val transactionId = repository.register(this)
+            repository.register(process.paymentItem.amountItem, transactionId)
+            repository.register(process.instrumentItem.cardItem, transactionId)
+            repository.register(process.payerItem, transactionId)
+            repository.register(this.statusItem, transactionId)
+        }
+    }
+
 }
