28531bd11d3cd7f6b53e6a9a4ca59d56bd0f6a10
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-17 17:00:36 -0500

    refactor: add try transaction

--- app/src/main/java/dev/adds/placetopay/provider/repository/PaymentRepository.kt
+++ app/src/main/java/dev/adds/placetopay/provider/repository/PaymentRepository.kt
@@ -1,17 +1,19 @@
 package dev.adds.placetopay.provider.repository
 
+import android.util.Log
+import com.google.gson.Gson
 import dev.adds.placetopay.model.database.dao.TransactionDao
 import dev.adds.placetopay.model.database.entities.*
+import dev.adds.placetopay.model.domain.payment.ReferenceModel
 import dev.adds.placetopay.provider.PaymentProvider
-import dev.adds.placetopay.provider.ProcessProvider
 import dev.adds.placetopay.provider.services.ApiService
 import dev.adds.placetopay.usescase.model.*
+import dev.adds.placetopay.util.Constants
 import javax.inject.Inject
 
 class PaymentRepository @Inject constructor(
     private val api: ApiService,
     private val paymentProvider: PaymentProvider,
-    private val processProvider: ProcessProvider,
     private val dao: TransactionDao
 ) {
 
@@ -21,7 +23,7 @@ class PaymentRepository @Inject constructor(
 
     suspend fun register(response: ProcessResponseItem): Long {
         return dao.insert(TransactionEntity(provider = response.provider?: String(),
-            reference = response.reference))
+            reference = response.reference, internalReference = response.internalReference))
     }
     suspend fun register(amountItem: AmountItem, transactionId: Long): Long{
         return  dao.insert(AmountEntity(currency = amountItem.currency ,
@@ -46,16 +48,47 @@ class PaymentRepository @Inject constructor(
             reason = statusItem.reason,
             date = statusItem.date, transactionOwnerId = transactionId))
 
-    suspend fun getAllPayments(): List<ShoppingItem>{
-        return dao.getAllTransactions().map { it.toDomain() }
+    suspend fun getAllPayments(): List<ShoppingItem> =
+        dao.getAllTransactions().map { it.toDomain() }
+
+    suspend fun getAllPendingPayments(): MutableList<ShoppingItem> {
+        val pending = dao.getAllPendingTransactions()
+        return if(pending.isNullOrEmpty())
+            mutableListOf()
+        else
+            dao.getAllPendingTransactions()!!.map { it.toDomain() } as MutableList<ShoppingItem>
     }
+
 
     fun addPayment(shoppingItem: ShoppingItem){
         paymentProvider.shoppingModel.add(shoppingItem.toModel())
     }
 
     suspend fun removePayment(shoppingItem: ShoppingItem): Boolean {
-        val transaction = dao.getTransactionByReference(shoppingItem.processResponseItem.reference)
-        return dao.remove(transaction.transaction) > 0
+        val transaction = dao.getTransactionByReference(shoppingItem.processResponseItem.internalReference)
+        return dao.remove(transaction!!.transaction) > 0
+    }
+
+    private suspend fun updatePayment(shoppingItem: ShoppingItem): Boolean {
+        val transaction = dao.getTransactionByReference(shoppingItem.processResponseItem.internalReference)
+        return dao.insert(transaction!!.status.apply {
+            status = shoppingItem.processResponseItem.statusItem.status
+            reason = shoppingItem.processResponseItem.statusItem.reason
+            message = shoppingItem.processResponseItem.statusItem.message
+            date = shoppingItem.processResponseItem.statusItem.date
+        }) > 0
+    }
+
+    suspend fun tryTransaction(shoppingItem: ShoppingItem): ShoppingItem {
+        if(shoppingItem.processResponseItem.statusItem.status != Constants.StatusResponse.PROCESSING.status){
+            val response = api.getQueryResponse(ReferenceModel(shoppingItem.processResponseItem.internalReference))
+            Log.i("tryTransaction", Gson().toJson(response))
+            shoppingItem.processResponseItem.statusItem = response.statusModel.toDomain()
+            updatePayment(shoppingItem)
+        }
+        else
+            Log.i(Constants.DEBUG_KEY, "Register with getProcessResponse")
+
+        return shoppingItem
     }
 }
