ea95a8caff067582ebd433c9163e484e8ab56e4b
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-17 09:59:17 -0500

    refactor: update cart view model

--- app/src/main/java/dev/adds/placetopay/ui/cart/CartViewModel.kt
+++ app/src/main/java/dev/adds/placetopay/ui/cart/CartViewModel.kt
@@ -3,10 +3,11 @@ package dev.adds.placetopay.ui.cart
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.adds.placetopay.model.domain.ProductModel
 import dev.adds.placetopay.usescase.ManagementCart
 import dev.adds.placetopay.usescase.model.ProductItem
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,30 +15,46 @@ class CartViewModel @Inject constructor(
     private  val managementCart: ManagementCart
     ) : ViewModel() {
 
-    private val productsList  = MutableLiveData<List<ProductItem>>()
+    private val productsList  = MutableLiveData<List<ProductItem>?>()
     private val productItem = MutableLiveData<ProductItem>()
     private val totalCart = MutableLiveData<Float>()
 
-    val products : LiveData<List<ProductItem>> = productsList
+    val products : MutableLiveData<List<ProductItem>?> = productsList
     val productModel: LiveData<ProductItem> = productItem
     val total : LiveData<Float> = totalCart
 
 
     fun onCreate(){
-        productsList.value = managementCart()!!
-        computedTotal()
+        viewModelScope.launch {
+            productsList.value = managementCart()
+            computedTotal()
+        }
+    }
+    private fun getCart(){
+        viewModelScope.launch {
+            productsList.value = managementCart.getCart()
+            computedTotal()
+        }
     }
     fun addProduct(productItem: ProductItem){
-        managementCart.addProduct(productItem)
-        onCreate()
+        viewModelScope.launch {
+            managementCart.addProduct(productItem)
+            getCart()
+        }
     }
 
-    fun removeProduct(productItem: ProductItem): Boolean {
-        var flag = managementCart.removeProduct(productItem)
-        onCreate()
-        return flag
+    fun removeProduct(productItem: ProductItem): Unit {
+        viewModelScope.launch {
+            managementCart.removeProduct(productItem)
+            getCart()
+        }
     }
-    fun clean() : Unit = managementCart.clean()
+    fun clean() : Unit {
+        viewModelScope.launch {
+            managementCart.clean()
+            getCart()
+        }
+    }
 
     private fun computedTotal(){
         totalCart.postValue(managementCart.computedTotal())
